ffcd9897ed6a9886bc9049e9b6cb36e161f85f3e
Juki0204 <jukid0204@yahoo.co.jp>
2025-05-04 01:39:43 +0900

    build phaser bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    rollupOptions: {
-      external: ['phaser'] // Phaser本体はCDNで読み込むため除外
-    },
     assetsInlineLimit: 0,
   },
   base: '/jump-master/',

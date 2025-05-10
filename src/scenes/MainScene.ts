b47042ec41095ddc24fd8126ffaa7d4d80a0e7e6
Juki0204 <jukid0204@yahoo.co.jp>
2025-05-11 01:07:46 +0900

    スコア表示・ゲームオーバー処理の追加、調整

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -8,7 +8,6 @@ class MainScene extends Phaser.Scene {
   private escKey!: Phaser.Input.Keyboard.Key;
   private player!: Player;
   private platform!: Phaser.Physics.Arcade.StaticGroup;
-  private currentGround: any | null = null;
   private currentGroundY: number = 4000;
   private logFlg = false;
   private playerX: number | null = null;
@@ -18,29 +17,9 @@ class MainScene extends Phaser.Scene {
   private mapWidth: number = 1280;
   private mapHeight: number = 4000;
 
-  // private destroyBlocksBelowCurrentGround(): void {
-  //   const destroyBlock = this.platform.children.entries.filter(block =>
-  //     (block as Phaser.Physics.Arcade.Sprite).y - 200 > this.currentGroundY &&
-  //     this.currentGroundY > 4000
-  //   );
-  //   console.log('今からブロック消えます');
-  //   console.log(destroyBlock);
-
-  //   destroyBlock.forEach(block => {
-  //     const sprite = block as Phaser.Physics.Arcade.Sprite;
-
-  //     this.tweens.add({
-  //       targets: sprite,
-  //       alpha: 0,
-  //       duration: 500,
-  //       ease: 'Cubic.easeIn',
-  //       onComplete: () => {
-  //         sprite.disableBody(true, true);
-  //         sprite.destroy();
-  //       }
-  //     });
-  //   });
-  // }
+  //UI
+  private score: number = this.mapHeight - this.currentGroundY;
+  private uiScore!: Phaser.GameObjects.Text;
 
   constructor() {
     super({ key: 'MainScene' });
@@ -57,30 +36,27 @@ class MainScene extends Phaser.Scene {
     this.spaceKey = this.input!.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     this.escKey = this.input!.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
 
+    //UI
+    this.uiScore = this.add.text(10, 10, `登った高さ:${this.score}m`, { fontSize: 32, fontStyle: 'bold', padding: { x: 10, y: 10 } }).setOrigin(0, 0);
+    this.uiScore.setScrollFactor(0);
+
     this.platform = this.physics.add.staticGroup();
 
     //地面の生成
     for (let x = 0; x < this.mapWidth; x += 64) {
-      this.platform.create(x, 4000, 'maps', 'terrain_grass_block_top').setScale(1, 1).setOrigin(0, 1).refreshBody();
+      this.platform.create(x, this.mapHeight, 'maps', 'terrain_grass_block_top').setScale(1, 1).setOrigin(0, 1).refreshBody();
     }
-    // platform.create(640, 800, 'ground').setScale(4.0, 1).setOrigin(0.5, 1).refreshBody();
-    // this.platform.create(400, 3850, 'ground').setScale(0.3, 0.8).setOrigin(0.5, 1).refreshBody();
-    // this.platform.create(680, 3700, 'ground').setScale(0.3, 0.8).setOrigin(0.5, 1).refreshBody();
-    // this.platform.create(1000, 3650, 'ground').setScale(0.2, 0.8).setOrigin(0.5, 1).refreshBody();
-    // this.platform.create(1200, 3550, 'ground').setScale(0.2, 0.8).setOrigin(0.5, 1).refreshBody();
-    new GroundBlock(this, this.platform, 360, 3750, 3, 'right', 'grass');
-    new GroundBlock(this, this.platform, 640, 3550, 3, 'right', 'grass');
-    // new GroundBlock(this, this.platform, 950, 3550, 2, 'grass');
-    // new GroundBlock(this, this.platform, 1150, 3400, 2, 'grass');
+
+    new GroundBlock(this, this.platform, 360, this.mapHeight - 250, 3, 'right', 'grass');
+    new GroundBlock(this, this.platform, 640, this.mapHeight - 450, 3, 'right', 'grass');
 
     this.cameras.main.setBounds(0, 0, this.mapWidth, this.mapHeight); //カメラの移動範囲
-    this.physics.world.setBounds(0, 0, this.mapWidth, this.mapHeight) //物理演算の範囲
+    this.physics.world.setBounds(0, 0, this.mapWidth, this.mapHeight); //物理演算の範囲
 
     Player.createAnims(this);
 
-    this.player = new Player(this, 100, 3950);
+    this.player = new Player(this, 100, this.mapHeight - 50);
     this.physics.add.collider(this.player.sprite, this.platform, (_playerObj, groundObj) => {
-      this.currentGround = groundObj;
       this.currentGroundY = (groundObj as Phaser.Physics.Arcade.Sprite).y;
     });
 
@@ -104,8 +80,7 @@ class MainScene extends Phaser.Scene {
     //ランダム足場生成
     if ((this.player.sprite.body! as Phaser.Physics.Arcade.Body).onFloor()) {
       if (this.logFlg === false) {
-        console.log('着地なう');
-        console.log('今接地しているのは:', this.currentGround.texture.key, this.currentGround.frame.name);
+        // console.log('着地なう');
         console.log('今接地している足場の高さは:', this.currentGroundY);
         this.logFlg = true;
         this.playerX = (this.player.sprite.body as Phaser.Physics.Arcade.Body).x;
@@ -113,17 +88,19 @@ class MainScene extends Phaser.Scene {
 
         console.log(this.platform.children.entries.filter(block => (block as Phaser.Physics.Arcade.Sprite).y === this.currentGroundY)); //今乗ってる足場
 
-        //自分の高さより200px以上下にある地面以外の足場の削除
-        let destroyBlock = this.platform.children.entries.filter(block => (block as Phaser.Physics.Arcade.Sprite).y - 200 > this.currentGroundY);
-        console.log('今からブロック消えます');
-        console.log(destroyBlock);
+        //自分の着地した足場より200px以上下にある足場の削除（最下層の地面は除外
+        let destroyBlock = this.platform.children.entries.filter(block => (block as Phaser.Physics.Arcade.Sprite).y - 200 > this.currentGroundY && (block as Phaser.Physics.Arcade.Sprite).y < this.mapHeight);
+        // console.log('今からブロック消えます');
+        // console.log(destroyBlock);
         destroyBlock.forEach(block => {
           const sprite = block as Phaser.Physics.Arcade.Sprite;
 
+          //足場落下アニメーション
           this.tweens.add({
             targets: sprite,
             alpha: { from: 1, to: 0 },
-            duration: 500,
+            y: sprite.y + 50,
+            duration: 300,
             // delay: 300,
             ease: 'Cubic.easeIn',
             onComplete: () => {
@@ -131,22 +108,24 @@ class MainScene extends Phaser.Scene {
             }
           });
         });
-        // this.destroyBlocksBelowCurrentGround();
 
+        //現在の足場と同時に生成された足場の2つをカウント
         let currentMoreBlock = this.platform.children.entries.filter(block => (block as Phaser.Physics.Arcade.Sprite).y <= this.currentGroundY);
         let currentMoreBlockY = currentMoreBlock.map(obj => (obj as Phaser.Physics.Arcade.Sprite).y);
         let currentMoreBlockCount = new Set(currentMoreBlockY).size
         console.log(currentMoreBlockCount);
 
-        if (currentMoreBlockCount <= 2 && this.currentGroundY < 3600) {
+        //現在の足場と同時に生成された足場のどちらかに乗った時点で次の足場を生成する（地面と初期生成のブロックは除外）
+        if (currentMoreBlockCount <= 2 && this.currentGroundY < this.mapHeight - 400) {
           const randomX = blockCreateRondomX(this.playerX);
           console.log(randomX);
 
           for (let i = 1; i <= 2; i++) {
-            const randomY = Math.floor(Math.random() * (240 - 100 + 1)) + 100;
+            const randomY = Math.floor(Math.random() * (240 - 100 + 1)) + 100; //恐らくジャンプで届く最大高度
             const randomLength = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-            // const dir = randomX < this.playerX ? 'left' : 'right';
             console.log(randomY);
+
+            //着地点から見て左右に1つずつ生成する
             if (i === 1) {
               new GroundBlock(this, this.platform, randomX[0], this.currentGroundY - randomY, randomLength, 'left');
             } else {
@@ -162,10 +141,15 @@ class MainScene extends Phaser.Scene {
     //ゲームオーバー処理
     if ((this.player.sprite.body as Phaser.Physics.Arcade.Body).y && (this.player.sprite.body as Phaser.Physics.Arcade.Body).y - this.currentGroundY > 300) {
       this.add.rectangle(0, 0, this.mapWidth, this.mapHeight, 0x000000, 0.8).setDepth(10).setOrigin(0, 0);
-      const gameoverTxt = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'GAME OVER', { fontSize: 40, fontStyle: 'bold' }).setOrigin(0.5, 0.5).setDepth(10);
+      const gameoverTxt = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'GAME OVER', { fontSize: 64, fontStyle: 'bold' }).setOrigin(0.5, 0.5).setDepth(10);
+      const gameoverScore = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2 + 60, `今回のスコア：${this.score}m`, { fontSize: 32, fontStyle: 'bold', padding: { x: 10, y: 10 } }).setOrigin(0.5, 0.5).setDepth(10);
       gameoverTxt.setScrollFactor(0);
+      gameoverScore.setScrollFactor(0);
       this.scene.pause();
     }
+
+    this.score = this.mapHeight - this.currentGroundY;
+    this.uiScore.setText(`登った高さ:${this.score}m`);
   }
 }
 
@@ -174,7 +158,7 @@ function blockCreateRondomX(
   min = 0,
   max = 1280,
   excludeRange = 200,
-  limitRange = 290
+  limitRange = 290 //恐らくジャンプで届く最大距離
 ): number[] {
   const leftMin = Math.max(min, playerX - limitRange);
   const leftMax = Math.max(min, playerX - excludeRange);
@@ -185,21 +169,7 @@ function blockCreateRondomX(
   const leftWidth = leftMax - leftMin;
   const rightWidth = rightMax - rightMin;
 
-  // if (leftWidth <= 0 && rightWidth <= 0) {
-  //   console.warn('有効なX座標の範囲がありません');
-  //   return playerX;
-  // }
-
-  // どちら側を使うかを、範囲の広さに応じてランダムに選択（偏りを避ける）
-  // const totalWidth = leftWidth + rightWidth;
-  // const rand = Math.random() * totalWidth;
-
-  // if (rand < leftWidth) {
-  //   return Math.floor(Math.random() * leftWidth) + leftMin;
-  // } else {
-  //   return Math.floor(Math.random() * rightWidth) + rightMin;
-  // }
-
+  //自分の着地位置から左右に1か所ずつ足場生成用のX座標を生成
   return [Math.floor(Math.random() * leftWidth) + leftMin, Math.floor(Math.random() * rightWidth) + rightMin];
 }
 
